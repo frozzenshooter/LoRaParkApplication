6cc238d1efe479f6d7ca14c2f1c3d33ecf95e1e0
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-03-05 08:52:25 +0100

    Inital setup of the OpenStreetMap for Android: map on sensor overview activity added with possible custom pins

--- app/src/main/java/de/uniulm/loraparkapplication/SensorOverviewActivity.java
+++ app/src/main/java/de/uniulm/loraparkapplication/SensorOverviewActivity.java
@@ -3,9 +3,29 @@ package de.uniulm.loraparkapplication;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SensorOverviewActivity extends AppCompatActivity {
 
@@ -19,6 +39,118 @@ public class SensorOverviewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
+        }
+
+        MapView mMapView = (MapView) findViewById(R.id.map);
+
+        // OSM Map Initialize
+        //important! set your user agent to prevent getting banned from the osm servers
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+
+        mMapView.setTileSource(TileSourceFactory.MAPNIK);
+        //mMapView.setTileSource(TileSourceFactory.PUBLIC_TRANSPORT);
+        // Setup MapView
+        setupMapView(mMapView);
+        createPin(mMapView);
+    }
+
+    private void createPin(MapView mMapView){
+        GeoPoint geoPoint = new GeoPoint(37779300,-122419200);
+
+        OverlayItem overlayItem = new OverlayItem("San Fransisco", "California", geoPoint);
+        Drawable markerDrawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.marker_default);
+        overlayItem.setMarker(markerDrawable);
+
+        ArrayList<OverlayItem> overlayItemArrayList = new ArrayList<>();
+        overlayItemArrayList.add(overlayItem);
+        ItemizedOverlay<OverlayItem> locationOverlay = new ItemizedIconOverlay<>(overlayItemArrayList, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int i, OverlayItem overlayItem) {
+
+                Toast.makeText(SensorOverviewActivity.this, "Item's Title : "+overlayItem.getTitle() +"\nItem's Desc : "+overlayItem.getSnippet(), Toast.LENGTH_SHORT).show();
+                return true; // Handled this event.
+            }
+
+            @Override
+            public boolean onItemLongPress(int i, OverlayItem overlayItem) {
+                return false;
+            }
+        }, getApplicationContext());
+
+        mMapView.getOverlays().add(locationOverlay);
+    }
+
+
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+
+
+    private void checkPermissions() {
+
+        List<String> permissions = new ArrayList<String>();
+        String message = "OSMDroid permissions:";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nStorage access to store map tiles.";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nLocation to show user location.";
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        } // else: We already have permissions, so handle as normal
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<String, Integer>();
+                // Initial
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for ACCESS_FINE_LOCATION and WRITE_EXTERNAL_STORAGE
+                Boolean location = perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                Boolean storage = perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                if (location && storage) {
+                    // All Permissions Granted
+                    Toast.makeText(SensorOverviewActivity.this, "All permissions granted", Toast.LENGTH_SHORT).show();
+                } else if (location) {
+                    Toast.makeText(this, "Storage permission is required to store map tiles to reduce data usage and for offline usage.", Toast.LENGTH_LONG).show();
+                } else if (storage) {
+                    Toast.makeText(this, "Location permission is required to show the user's location on map.", Toast.LENGTH_LONG).show();
+                } else { // !location && !storage case
+                    // Permission Denied
+                    Toast.makeText(SensorOverviewActivity.this, "Storage permission is required to store map tiles to reduce data usage and for offline usage." +
+                            "\nLocation permission is required to show the user's location on map.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void setupMapView(MapView mMapView){
+        // Zoom buttons
+        mMapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
+        // Multitouch Controls Activation
+        mMapView.setMultiTouchControls(true);
+        mMapView.setClickable(true);
+        // Default map zoom level:
+        int MAP_DEFAULT_ZOOM = 20;
+        mMapView.getController().setZoom(MAP_DEFAULT_ZOOM);
+        // Default Point
+        GeoPoint startPoint = new GeoPoint(37779300, -122419200);
+        mMapView.getController().setCenter(startPoint);
     }
 
     @Override
