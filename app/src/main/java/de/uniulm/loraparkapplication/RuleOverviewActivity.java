9d3eff467bf96f990ce9a9181999593d0756eea6
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-03-09 08:38:20 +0100

    Inital RuleOverviewActivity setup (currently not working)

--- app/src/main/java/de/uniulm/loraparkapplication/RuleOverviewActivity.java
+++ app/src/main/java/de/uniulm/loraparkapplication/RuleOverviewActivity.java
@@ -3,9 +3,15 @@ package de.uniulm.loraparkapplication;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class RuleOverviewActivity extends AppCompatActivity {
 
@@ -20,6 +26,14 @@ public class RuleOverviewActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        //Attach the SectionsPagerAdapter to the ViewPager
+        RuleSectionsPagerAdapter pagerAdapter = new RuleSectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        //Attach the ViewPager to the TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
@@ -31,6 +45,43 @@ public class RuleOverviewActivity extends AppCompatActivity {
             default:
 
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class RuleSectionsPagerAdapter extends FragmentPagerAdapter {
+        public RuleSectionsPagerAdapter(FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    //TODO ;
+                case 1:
+                    //TODO:;
+                case 2:
+                    //TODO: ;
+            }
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getText(R.string.label_tab_active_rules);
+                case 1:
+                    return getResources().getText(R.string.label_tab_inactive_rules);
+                case 2:
+                    return getResources().getText(R.string.label_tab_all_rules);
+            }
+            return null;
         }
     }
 }
